0fdbf04887b31ce3bb4ac33185746e3fd9ad2ccd
AY2025NRes <addadi.yacine@gmail.com>
2025-02-25 23:16:34 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+fruits ={"pomme":"rouge", "banane":"jaune", "orange":"orange"}
+fruits["kiwi"]="vert"
+print(fruits)
+fruits ["banane"]="couleur_banane"
+fruits["pomme"]="vert"
+print(fruits)
+del fruits["banane"]
+print(fruits)
+print (fruits.keys())
